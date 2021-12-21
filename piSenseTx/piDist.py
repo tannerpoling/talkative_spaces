1edb6eed06535e6235ada3744b4649bcb3af63c9
Tanner Poling <tannerpoling@gmail.com>
2021-12-21 15:06:33 -0800

    radio transmitter test

---
+++ piSenseTx/piDist.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from time import sleep
+from gpiozero import DistanceSensor
+
+dist_sensor = DistanceSensor(echo=22, trigger=27, max_distance=4)
+
+print("Press CTRL-C to exit.\n")
+while True:
+    print("Distance sensor read %.1f cm." % (dist_sensor.distance * 100))
+    sleep(1)
